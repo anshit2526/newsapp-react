fd659712c9bc7a5c5d3fc0eee7ff28749e402cbf
Anshit Krishan <anshitsiddharth2003@gmail.com>
2024-01-10 17:05:38 +0530

    Added infinite scroll, removed Next and Previous buttons

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export default class News extends Component {
 
@@ -23,7 +24,7 @@ export default class News extends Component {
             articles: [],
             loading: false,
             page: 1,
-            totalResults: []
+            totalResults: 0,
         }
         document.title = `${this.capitalize(this.props.category)} | NewsMonkey`;
     }
@@ -44,42 +45,68 @@ export default class News extends Component {
         this.updateNews();
     }
 
-    handlePrevClick = async () => {
-        this.setState({
-            page: this.state.page - 1
-        })
-        this.updateNews()
-    }
+    // handlePrevClick = async () => {
+    //     this.setState({
+    //         page: this.state.page - 1
+    //     })
+    //     this.updateNews()
+    // }
 
-    handleNextClick = async () => {
-        this.setState({
-            page: this.state.page + 1
-        })
-        this.updateNews()
-    }
+    // handleNextClick = async () => {
+    //     this.setState({
+    //         page: this.state.page + 1
+    //     })
+    //     this.updateNews()
+    // }
 
     capitalize(string) {
         return string.charAt(0).toUpperCase() + string.substring(1, string.lengthS)
     }
 
+
+    fetchMoreData = async () => {
+        this.setState({
+            page: this.state.page + 1
+        })
+
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2225fe2477514334b282e55903db9ac1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+        })
+    };
+
     render() {
         return (
-            <div className='container my-3'>
+            <>
                 <h1 className='text-center' >NewsMonkey - Top {this.capitalize(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                        </div>
-                    })}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
 
-                </div>
-                <div className="container d-flex justify-content-between">
+                    <div className="container">
+                        <div className="row">
+                            {!this.state.loading && this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
+
+                        </div>
+                    </div>
+
+                </InfiniteScroll>
+                {/* <div className="container d-flex justify-content-between">
                     <button id='prev-btn' disabled={this.state.page <= 1} type='button' className="btn btn-primary" onClick={this.handlePrevClick}>&larr; Previous</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 9)} id='next-btn' type='button' className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div>
+                </div> */}
+            </>
         )
     }
 }
